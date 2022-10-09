21e55f8eeef496c59f9dbc9d73463946cc7c69f1
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2022-10-09 13:45:13 +0200

    City and Country Service

---
+++ src/main/java/com/neirba/neirba/city/CityService.java
@@ -0,0 +1,37 @@
+package com.neirba.neirba.city;
+
+import com.neirba.neirba.exception.UserNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService {
+    private final CityRepo cityRepo;
+
+    public CityService(CityRepo cityRepo) {
+        this.cityRepo = cityRepo;
+    }
+
+    public City addCity(City city) {
+        return cityRepo.save(city);
+    }
+
+    public List<City> findAllCities() {
+        return cityRepo.findAll();
+    }
+
+    public City findCityById(Long id) {
+        return cityRepo.findCityById(id).orElseThrow(() ->
+                new UserNotFoundException("City with id " + id + " does not exist"));
+    }
+
+    public City updateCity(City city) {
+        return cityRepo.save(city);
+    }
+
+    public void deleteCity(Long id) {
+        cityRepo.deleteById(id);
+    }
+
+}
