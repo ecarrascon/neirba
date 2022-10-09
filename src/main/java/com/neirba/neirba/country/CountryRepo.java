264b605665fa5bef621a4c97262354cf433bf0a3
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2022-10-09 13:34:09 +0200

    City and Country Repository

---
+++ src/main/java/com/neirba/neirba/country/CountryRepo.java
@@ -0,0 +1,9 @@
+package com.neirba.neirba.country;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CountryRepo extends JpaRepository<Country, Long> {
+    Optional<Country> findCountryById(Long id);
+}
