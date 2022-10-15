3fbe5b62b89b007736790a3684bea25f5cefeb8c
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2022-10-15 22:14:35 +0200

    OAuthResoruceServer config

--- src/main/java/com/neirba/neirba/config/SecurityConfig.java
+++ src/main/java/com/neirba/neirba/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,8 +18,10 @@ public class SecurityConfig {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests( auth -> auth
                         .anyRequest().authenticated()
-                )
+                ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults()).build();
     }
+
+
 }
