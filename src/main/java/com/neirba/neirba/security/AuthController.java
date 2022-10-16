ab11badfd752c64222590a106ee030c9e8b1e767
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2022-10-16 13:23:46 +0200

    Security Added (Auth spring security)

---
+++ src/main/java/com/neirba/neirba/security/AuthController.java
@@ -0,0 +1,20 @@
+package com.neirba.neirba.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        return tokenService.generateToken(authentication);
+    }
+}
